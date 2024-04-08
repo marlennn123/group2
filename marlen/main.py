729ca6ea198b73803aa5bf7f25543a74879d6e1f
M-Stark <55349494+M-Stark@users.noreply.github.com>
2024-04-08 16:27:16 +0600

    new folder

---
+++ marlen/main.py
@@ -0,0 +1,10 @@
+dfgdfg
+df = pd.DataFrame({"Player" : ["Sach","KingK","Zak"], "Role" : ["Master Batter","Run Machine","Speed Machine"]})#df is your dataframe
+#ColumnOne is the selected/displayed column.
+#ColumnTwo is the aggregated column.
+newDf = df.groupby("dfg
+df = pd.DataFrame({"Player" : ["Sach","KingK","Zak"], "Role" : ["Master Batter","Run Machine","Speed Machine"]})#df is your dataframe
+#ColumnOne is the selected/displayed column.
+#ColumnTwo is the aggregated column.
+newDf = df.groupby("df
+gsd")["ColumnTwo"].count()")["ColumnTwo"].count()
